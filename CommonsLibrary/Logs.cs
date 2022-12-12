d754689e0540752b026b834b41c318c730812afa
Alex O'Brien <alex8obrien@gmail.com>
2022-12-12 23:17:43 +0000

    Added Method summaries and param details

--- CommonsLibrary/Logs.cs
+++ CommonsLibrary/Logs.cs
@@ -6,20 +6,33 @@ namespace CommonsLibrary
 {
     public class Logs
     {
+        /// <summary>This method will create a log file if it doesn't exist and then it will write the log to the file.
+        ///It can also be called manually to write a custom log to the log file</summary>
+        /// <param name="logMessage">The message that will be written to the log</param>
         public static void LogWrite(string logMessage)
         {
             string pathString = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) ?? string.Empty;
             
-            if (!File.Exists(pathString + @"\Logs\log.txt"))
+            try
             {
-                var fs = File.Create(pathString + @"\Logs\log.txt");
-                fs.Close();
+                if (!File.Exists(pathString + @"\Logs\log.txt"))
+                {
+                    var fs = File.Create(pathString + @"\Logs\log.txt");
+                    fs.Close();
+                }
+            }
+            catch (IOException)
+            {
+                Console.WriteLine("Bruh even logging failed");
             }
     
             using StreamWriter writer = File.AppendText(pathString + @"\Logs\log.txt");
             { Log(logMessage, writer); }
         }
 
+        /// <summary>This method will write the log to the file</summary>
+        /// <param name="logMessage">The message that will be written to the log</param>
+        /// <param name="txtWriter">The class that will be used to write the log to the file</param>
         private static void Log(string logMessage, TextWriter txtWriter)
         { //This is the method that writes to the log file
             StringBuilder sb = new StringBuilder();
@@ -30,27 +43,16 @@ namespace CommonsLibrary
             txtWriter.WriteLine(sb.ToString());
         }
 
-        public static string LogBuilder(Exception e, string indent = "")
+        /// <summary>This will create a log for you with the exception information and then it will write the log to the file</summary>
+        /// <param name="e">The exception that occured</param>
+        public static void LogBuilder(Exception e)
         {
             StringBuilder sb = new StringBuilder();
-
-            if (indent.Length > 0)
-            {
-                sb.AppendFormat($"{indent}Inner ");
-            }
-
-            sb.AppendFormat($"Exception Found:\n{indent}Type: {e.GetType().FullName}");
-            sb.AppendFormat($"\n{indent}Message: {e.Message}");
-            sb.AppendFormat($"\n{indent}Source: {e.Source}");
-            sb.AppendFormat($"\n{indent}Stacktrace: {e.StackTrace}");
-
-            if (e.InnerException != null)
-            {
-                sb.Append("\n");
-                LogBuilder(sb, e.InnerException, indent + "  ");
-            }
-
-            return sb.ToString();
+            sb.AppendFormat($"Exception Found:\nType: {e.GetType().FullName}");
+            sb.AppendFormat($"\nMessage: {e.Message}");
+            sb.AppendFormat($"\nSource: {e.Source}");
+            sb.AppendFormat($"\nStacktrace: {e.StackTrace}");
+            LogWrite(sb.ToString());
         }
     }
 }
