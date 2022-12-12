a48ec6a03635e637b8444d3eed5dea4371289d1b
Alex O'Brien <alex8obrien@gmail.com>
2022-12-12 22:49:18 +0000

    initial logs class

---
+++ CommonsLibrary/Logs.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using System.Reflection;
+using System.Text;
+
+namespace CommonsLibrary
+{
+    public class Logs
+    {
+        public static void LogWrite(string logMessage)
+        {
+            string pathString = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) ?? string.Empty;
+            
+            if (!File.Exists(pathString + @"\Logs\log.txt"))
+            {
+                var fs = File.Create(pathString + @"\Logs\log.txt");
+                fs.Close();
+            }
+    
+            using StreamWriter writer = File.AppendText(pathString + @"\Logs\log.txt");
+            { Log(logMessage, writer); }
+        }
+
+        private static void Log(string logMessage, TextWriter txtWriter)
+        { //This is the method that writes to the log file
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Log Entry : ");
+            sb.AppendLine($"{DateTime.Now.ToLongDateString()}/{DateTime.Now.ToLongTimeString()}");
+            sb.AppendLine(": ");
+            sb.AppendLine($"{logMessage}");
+            txtWriter.WriteLine(sb.ToString());
+        }
+    }
+}
