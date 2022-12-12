066400ca6bba84392d4cae0385706c43002ca935
Alex O'Brien <alex8obrien@gmail.com>
2022-12-12 20:48:33 +0000

    first commit

---
+++ CommonsLibrary/StdInp.cs
@@ -0,0 +1,36 @@
+﻿namespace StdInp
+{
+    public class StdInp
+    {
+        public static string Input(string msg)
+        {
+            Console.Write($"{msg}: ");
+            return Console.ReadLine() ?? string.Empty;
+        }
+        public static int InputIntInBound(string msg, int lowerBound, int upperBound)
+        {
+            int output;
+            bool isInt;
+            do
+            {
+                isInt = int.TryParse(Input($"{msg} ({lowerBound}-{upperBound}) "), out output);
+            } while (!isInt && !(lowerBound <= output && output <= upperBound));
+            return output;
+        }
+        public static int InputInt(string msg)
+        {
+            int output;
+            bool isInt;
+            do
+            {
+                isInt = int.TryParse(Input(msg), out output);
+            } while (!isInt);
+            return output;
+        }
+        public static bool InputYNAsBool(string msg)
+        {
+            string inp = Input($"{msg} (y/n)").ToUpper();
+            return inp.Equals("Y");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+# Commons Library
+
+This is my commons library for all of my personal C# projects.
+
+## Index
+
+- [Standard Input / Output](/CommonsLibrary/StdInp.cs)
+
+## Contributors
+- [Alex O'Brien](https://github.com/alex8obrien)
+
+### *Licensed under the DBAD (dont be a dick) License, Find it [here](/LICENSE.md)*
