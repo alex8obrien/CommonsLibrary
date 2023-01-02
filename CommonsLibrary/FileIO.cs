29754998f89b833b408699432e88c3b01beb306e
Alex O'Brien <alex8obrien@gmail.com>
2023-01-02 11:18:49 +0000

    Added XML summary, LineAt and simplified TotalLines

--- CommonsLibrary/FileIO.cs
+++ CommonsLibrary/FileIO.cs
@@ -4,14 +4,23 @@ namespace CommonsLibrary
 {
     public class FileIO
     {
+        /// <summary>This methods returns the number of lines in a file.</summary>
+        /// <param name="filePath">It's the path of the file from the solution folder including the file extension</param>
+        /// <returns>An integer of the number of lines in a file</returns>
         public static int TotalLines(string filePath)
         {
-            using (StreamReader r = new(filePath))
-            {
-                int count = 0;
-                while (r.ReadLine() != null) { count++; }
-                return count;
-            }
+            using StreamReader r = new(filePath);
+            int count = 0;
+            while (r.ReadLine() != null) { count++; }
+            return count;
         }
+        
+        /// <summary>It returns a string of a specific line in a file</summary>
+        /// <param name="filePath">It's the path of the file from the solution folder including the file extension</param>
+        /// <param name="lineNumber">The specific line you want returned</param>
+        /// <returns>A line in a file as a string</returns>
+        /// <exception cref="Exception">New exception - "Line not found"</exception>
+        public static string LineAt(string filePath, int lineNumber) =>
+            File.ReadLines(filePath).ElementAt(lineNumber) ?? throw new Exception("Line not found");
     }
 }
