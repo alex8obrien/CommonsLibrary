409b48f3a424e238ef52409e33cc2522834c1765
Alex O'Brien <alex8obrien@gmail.com>
2023-01-22 22:28:44 +0000

    Code Refactor, changing arrays to readonly or enumerable.

--- CommonsLibrary/StdOut.cs
+++ CommonsLibrary/StdOut.cs
@@ -1,58 +1,59 @@
 using System;
+using System.Collections.Generic;
 
-namespace StdOut
+namespace CommonsLibrary
 {
     internal class StdOut
     {
         /// <summary>
         /// Creates a table from Headers and Data in Array format
-        /// <param name="Headers">Is the headers for the table in an array</param>
-        /// <param name="Data">Is the data for the table in an array</param>
+        /// <param name="headers">Is the headers for the table in an array</param>
+        /// <param name="data">Is the data for the table in an array</param>
         /// </summary>
-        public static void ArrayTable(string[] Headers, string[,] Data)
+        public static void ArrayTable(string[] headers, string[,] data)
         {
             // Makes max width for each column
-            int[] largest = new int[Headers.Length];
+            int[] largest = new int[headers.Length];
 
             // Makes the maximum width equal to each header
-            for (int i = 0; i < Headers.Length; i++)
-            { largest[i] = Headers[i].Length; }
+            for (int i = 0; i < headers.Length; i++)
+            { largest[i] = headers[i].Length; }
 
             // Makes the maximum width equal to each data if its bigger
-            for (int i = 0; i < Data.GetLength(0); i++)
+            for (int i = 0; i < data.GetLength(0); i++)
             {
-                for (int j = 0; j < Data.GetLength(1); j++)
+                for (int j = 0; j < data.GetLength(1); j++)
                 {
-                    if (Data[i, j].Length > largest[j])
+                    if (data[i, j].Length > largest[j])
                     {
-                        largest[j] = Data[i, j].Length;
+                        largest[j] = data[i, j].Length;
                     }
                 }
             }
 
-            WriteHeaders(Headers, largest);
+            WriteHeaders(headers, largest);
             Underline(largest);
             Console.Write("\n");
-            WriteData(Data, largest);
+            WriteData(data, largest);
         }
 
         /// <summary>
         /// Writes the headers for the table with equal spacing
         /// </summary>
-        /// <param name="Headers">The array containing the header strings</param>
-        /// <param name="MaxWidth">The maximum with for each column</param>
-        private static void WriteHeaders(string[] Headers, int[] MaxWidth)
+        /// <param name="headers">The array containing the header strings</param>
+        /// <param name="maxWidth">The maximum with for each column</param>
+        private static void WriteHeaders(IReadOnlyList<string> headers, IReadOnlyList<int> maxWidth)
         {
             // Writes the underline
             Console.Write("|");
-            for (int i = 0; i < MaxWidth.Length; i++)
+            for (int i = 0; i < maxWidth.Count; i++)
             {
                 Console.Write(" ");
 
-                if (MaxWidth[i] % 2 == 0)
+                if (maxWidth[i] % 2 == 0)
                 {
-                    int width = MaxWidth[i];
-                    width -= Headers[i].Length;
+                    int width = maxWidth[i];
+                    width -= headers[i].Length;
                     width /= 2;
 
                     // Write first half of spaces
@@ -60,9 +61,9 @@ namespace StdOut
                     { Console.Write(" "); }
 
                     // Write header
-                    Console.Write(Headers[i]);
+                    Console.Write(headers[i]);
 
-                    if (Headers[i].Length % 2 == 0)
+                    if (headers[i].Length % 2 == 0)
                     {
                         // Write second half of spaces
                         for (int j = 0; j < width; j++)
@@ -77,8 +78,8 @@ namespace StdOut
                 }
                 else
                 {
-                    int width = MaxWidth[i];
-                    width -= Headers[i].Length;
+                    int width = maxWidth[i];
+                    width -= headers[i].Length;
                     width /= 2;
 
                     // Write first half of spaces
@@ -86,9 +87,9 @@ namespace StdOut
                     { Console.Write(" "); }
 
                     // Write header
-                    Console.Write(Headers[i]);
+                    Console.Write(headers[i]);
 
-                    if (Headers[i].Length % 2 == 0)
+                    if (headers[i].Length % 2 == 0)
                     {
                         // Write second half of spaces
                         for (int j = 0; j <= width; j++)
@@ -109,14 +110,14 @@ namespace StdOut
         /// <summary>
         /// Underlines the headers in the table
         /// </summary>
-        /// <param name="MaxWidth">It's the maximum width of each column/param>
-        private static void Underline(int[] MaxWidth)
+        /// <param name="maxWidth">It's the maximum width of each column</param>
+        private static void Underline(IEnumerable<int> maxWidth)
         {
             // Writes the underline
             Console.Write("\n|");
-            for (int i = 0; i < MaxWidth.Length; i++)
+            foreach (int value in maxWidth)
             {
-                for (int j = 0; j < MaxWidth[i] + 2; j++)
+                for (int j = 0; j < value + 2; j++)
                 {
                     Console.Write("-");
                 }
@@ -127,42 +128,42 @@ namespace StdOut
         /// <summary>
         /// It writes the data to the console
         /// </summary>
-        /// <param name="Data">It's a 2D array that contains data for the table</param>
-        /// <param name="MaxWidth">It's the maximum width of each column</param>
-        private static void WriteData(string[,] Data, int[] MaxWidth)
+        /// <param name="data">It's a 2D array that contains data for the table</param>
+        /// <param name="maxWidth">It's the maximum width of each column</param>
+        private static void WriteData(string[,] data, IReadOnlyList<int> maxWidth)
         {
-            for (int i = 0; i < Data.GetLength(0); i++)
+            for (int i = 0; i < data.GetLength(0); i++)
             {
                 Console.Write("|");
-                for (int j = 0; j < Data.GetLength(1); j++)
+                for (int j = 0; j < data.GetLength(1); j++)
                 {
-                    if (Data[i, j].Length == MaxWidth[j])
+                    if (data[i, j].Length == maxWidth[j])
                     {
-                        Console.Write(" " + Data[i, j]);
+                        Console.Write(" " + data[i, j]);
                         Console.Write(" |");
                         continue;
                     }
 
-                    if (Data[i, j].Length % 2 == 0)
+                    if (data[i, j].Length % 2 == 0)
                     {
-                        int numberOfSpaces = MaxWidth[j] - Data[i, j].Length;
+                        int numberOfSpaces = maxWidth[j] - data[i, j].Length;
 
                         for (int k = 0; k < numberOfSpaces / 2; k++)
                         { Console.Write(" "); }
 
-                        Console.Write(Data[i, j]);
+                        Console.Write(data[i, j]);
 
                         for (int k = 0; k < numberOfSpaces / 2; k++)
                         { Console.Write(" "); }
                     }
                     else
                     {
-                        int numberOfSpaces = MaxWidth[j] - Data[i, j].Length;
+                        int numberOfSpaces = maxWidth[j] - data[i, j].Length;
 
                         for (int k = 0; k <= Math.Floor(numberOfSpaces / 2f); k++)
                         { Console.Write(" "); }
 
-                        Console.Write(Data[i, j]);
+                        Console.Write(data[i, j]);
 
                         for (int k = 0; k < Math.Ceiling(numberOfSpaces / 2f); k++)
                         { Console.Write(" "); }
