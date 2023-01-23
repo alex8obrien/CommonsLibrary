09da6f0e162c9f2d3e897a9a81d507b538659ba0
Alex O'Brien <alex8obrien@gmail.com>
2023-01-23 19:51:00 +0000

    Refactored TotalLines to remove data storage, removed whitespace and added TotalCharacters method

--- CommonsLibrary/FileIO.cs
+++ CommonsLibrary/FileIO.cs
@@ -11,11 +11,8 @@ namespace CommonsLibrary
         /// <returns>An integer of the number of lines in a file</returns>
         public static int TotalLines(string filePath)
         {
-            filePath =  Directory.GetParent(Directory.GetCurrentDirectory().Split("\\bin")[0]) + "\\" +  filePath;
-            using StreamReader reader = new(filePath);
-            int count = 0;
-            while (reader.ReadLine() != null) { count++; }
-            return count;
+            filePath = Directory.GetParent(Directory.GetCurrentDirectory().Split("\\bin")[0]) + "\\" + filePath;
+            return File.ReadAllLines(filePath).Length;
         }
 
         /// <summary>It returns a string of a specific line in a file from a 0 based index</summary>
@@ -25,8 +22,17 @@ namespace CommonsLibrary
         /// <exception cref="Exception">New exception - "Line not found"</exception>
         public static string LineAt(string filePath, int lineNumber)
         {
-            filePath =  Directory.GetParent(Directory.GetCurrentDirectory().Split("\\bin")[0]) + "\\" +  filePath;
+            filePath = Directory.GetParent(Directory.GetCurrentDirectory().Split("\\bin")[0]) + "\\" + filePath;
             return File.ReadLines(filePath).ElementAt(lineNumber) ?? throw new Exception("Line not found");
+        }
+
+        /// <summary>It returns an integer of all the characters in a file</summary>
+        /// <param name="filePath">It's the path of the file from the solution folder including the file extension</param>
+        /// <returns>An integer of all the characters in a file</returns>
+        public static int TotalChars(string filePath)
+        {
+            filePath = Directory.GetParent(Directory.GetCurrentDirectory().Split("\\bin")[0]) + "\\" + filePath;
+            return File.ReadAllLines(filePath).Sum(line => line.Length);
         }
     }
 }
