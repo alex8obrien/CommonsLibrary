6cb0b9972f858e1bbf8ba1f85dea8861e540ebdd
Alex O'Brien <alex8obrien@gmail.com>
2023-02-09 23:29:51 +0000

    Moved Logs to own project

---
+++ CommonsLibrary.Logging/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CommonsLibrary.Logging
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
