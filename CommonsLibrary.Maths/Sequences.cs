c30774ef1fb36846661ace3077c7a3aa0ecc432b
Alex O'Brien <alex8obrien@gmail.com>
2023-03-03 10:56:30 +0000

    Made not implemented methods

--- CommonsLibrary.Maths/Sequences.cs
+++ CommonsLibrary.Maths/Sequences.cs
@@ -4,6 +4,24 @@ namespace CommonsLibrary.Maths
 {
     public static class Sequences
     {
-        
+        public static int NthFibonacciValue(int index)
+        {
+            return NotImplementedException;
+        }
+
+        public static int[] FibonacciValues(int count)
+        {
+            return NotImplementedException;
+        }
+
+        public static int NthPrimeNumber(int index)
+        {
+            return NotImplementedException;
+        }
+
+        public static int[] PrimeNumbers(int count)
+        {
+            return NotImplementedException;
+        }
     }
 }
