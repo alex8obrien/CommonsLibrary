91f8ee0d0fb99c5eca57887e8a481a6c01e713f9
Alex O'Brien <alex8obrien@gmail.com>
2023-03-03 22:57:33 +0000

    Added Fibonacci methods

--- CommonsLibrary.Maths/Sequences.cs
+++ CommonsLibrary.Maths/Sequences.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
 
 namespace CommonsLibrary.Maths
 {
@@ -6,12 +8,28 @@ namespace CommonsLibrary.Maths
     {
         public static int NthFibonacciValue(int index)
         {
-            return NotImplementedException;
+            if (index == 0) return 0;
+
+            int prev = 0, next = 1;
+            for (int i = 1; i < index; i++)
+            {
+                int sum = prev + next;
+                prev = next;
+                next = sum;
+            }
+            return next;
         }
 
-        public static int[] FibonacciValues(int count)
+        public static int[] FibonacciValues(int sequenceLength)
         {
-            return NotImplementedException;
+            int[] fibonacci = new int[sequenceLength];
+            fibonacci[0] = 0;
+            fibonacci[1] = 1;
+            for (int index = 2; index < sequenceLength; index++)
+            {
+                fibonacci[index] = fibonacci[index - 2] + fibonacci[index - 1];
+            }
+            return fibonacci;
         }
 
         public static int NthPrimeNumber(int index)
