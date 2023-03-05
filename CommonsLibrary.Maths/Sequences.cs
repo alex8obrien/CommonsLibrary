fb2b0ff2124ff21dca94cddcc6e31527a8a0d9ca
Alex O'Brien <alex8obrien@gmail.com>
2023-03-05 13:30:45 +0000

    Added XML Data

--- CommonsLibrary.Maths/Sequences.cs
+++ CommonsLibrary.Maths/Sequences.cs
@@ -2,8 +2,12 @@
 
 namespace CommonsLibrary.Maths
 {
+    /// <summary>A class containing methods regarding sequences.</summary>
     public static class Sequences
     {
+        /// <summary>It calculates the Nth value in the Fibonacci sequence.</summary>
+        /// <param name="index">The index of the value you want to return.</param>
+        /// <returns>An integer that's the Nth Fibonacci value.</returns>
         public static int NthFibonacciValue(int index)
         {
             if (index == 0) return 0;
@@ -18,6 +22,9 @@ namespace CommonsLibrary.Maths
             return next;
         }
 
+        /// <summary>Calculates an array of Fibonacci values between two parameters.</summary>
+        /// <param name="sequenceLength">The number of values you want to return.</param>
+        /// <returns>An array of Fibonacci values.</returns>
         public static int[] FibonacciValues(int sequenceLength)
         {
             int[] fibonacci = new int[sequenceLength];
@@ -30,6 +37,9 @@ namespace CommonsLibrary.Maths
             return fibonacci;
         }
 
+        /// <summary>It calculates the Nth prime number.</summary>
+        /// <param name="index">The index of the prime number you want.</param>
+        /// <returns>An integer of the calculated prime number.</returns>
         public static int NthPrimeNumber(int index)
         {
             int count = 0;
@@ -50,6 +60,10 @@ namespace CommonsLibrary.Maths
             return value;
         }
 
+        /// <summary>It calculates an array of prime numbers between two values.</summary>
+        /// <param name="lowBoundary">The lower bound range</param>
+        /// <param name="highBoundary">The upper bound range</param>
+        /// <returns>An array of prime integers between the bounds.</returns>
         public static int[] PrimeNumbers(int lowBoundary, int highBoundary)
         {
             List<int> list = new();
