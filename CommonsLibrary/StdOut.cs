00c38e51ccdeff84b38e21b13ad84a2027759c64
Alex O'Brien <alex8obrien@gmail.com>
2023-05-17 16:14:08 +0100

    Added Write/Line colour messages

--- CommonsLibrary/StdOut.cs
+++ CommonsLibrary/StdOut.cs
@@ -5,6 +5,19 @@ namespace CommonsLibrary
     /// <summary>Contains methods for standard output data.</summary>
     public static class StdOut
     {
+        /// <summary>Used to write messages to the console in colour without a new line</summary>
+        /// <param name="msg"></param>
+        /// <param name="colour"></param>
+        public static void WriteColourMessage(string msg, ConsoleColor colour)
+        {
+            Console.ForegroundColor = colour;
+            Console.Write(msg);
+            Console.ResetColor();
+        }
 
+        /// <summary>Used to write messages to the console in colour with a new line</summary>
+        /// <param name="msg"></param>
+        /// <param name="colour"></param>
+        public static void WriteLineColourMessage(string msg, ConsoleColor colour) => WriteColourMessage($"{msg}\n", colour);
     }
 }
