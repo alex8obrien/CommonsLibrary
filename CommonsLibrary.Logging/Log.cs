36b08b0480bceecb81b44bab1fba45cd21c54fd9
Alex O'Brien <56505884+alex8obrien@users.noreply.github.com>
2023-12-15 13:44:05 +0000

    Logging/v2 (#6)
    * Removed old log code
    
    * Rewritten log class
    
    * version number and moved to net standard 2.1
    
    * Updated README.md

--- CommonsLibrary.Logging/Log.cs
+++ CommonsLibrary.Logging/Log.cs
@@ -1,16 +1,36 @@
-﻿namespace CommonsLibrary.Logging
+﻿using System;
+using System.IO;
+
+namespace CommonsLibrary.Logging
 {
-    public class Log : LogBuilder
+    public class Log
     {
-        public Log(string error, string logMessage)
+        private readonly string _filePath;
+        private static Log? _logger;
+
+        private Log(string filePath) => _filePath = filePath;
+        public static Log GetLogger(string filePath) => _logger ??= new Log(filePath);
+
+        private void WriteLog(LogLevel level, string message, Exception? exception = null)
         {
-            log = new Logs("LOG", error,  logMessage);
+            using StreamWriter writer = new StreamWriter(_filePath, true);
+            writer.WriteLine($"{DateTime.Now} {level} {exception} {message}");
         }
 
-        public override void BuildLogMessage()
-        { }
+        public void Info(string message) =>
+            WriteLog(LogLevel.Info, message);
+            
+        public void Warning(string message) =>
+            WriteLog(LogLevel.Warning, message);
 
-        public override void BuildError()
-        { }
+        public void Error(string message, Exception exception) =>
+            WriteLog(LogLevel.Error, message, exception);
+        
+        private enum LogLevel
+        {
+            Info,
+            Warning,
+            Error,
+        }
     }
 }
--- CommonsLibrary.Logging/README.md
+++ CommonsLibrary.Logging/README.md
@@ -5,11 +5,6 @@ This is my C# logging library for all of my personal C# projects.
 ## Index
 
 - [Log](https://github.com/alex8obrien/CommonsLibrary/blob/master/CommonsLibrary.Logging/Log.cs)
-- [Warning](https://github.com/alex8obrien/CommonsLibrary/blob/master/CommonsLibrary.Logging/Warning.cs)
-- [Error](https://github.com/alex8obrien/CommonsLibrary/blob/master/CommonsLibrary.Logging/Error.cs)
-- [Builder Class](https://github.com/alex8obrien/CommonsLibrary/blob/master/CommonsLibrary.Logging/LogBuilder.cs)
-- [Director Class](https://github.com/alex8obrien/CommonsLibrary/blob/master/CommonsLibrary.Logging/LogDirector.cs)
-- [Base Log Class](https://github.com/alex8obrien/CommonsLibrary/blob/master/CommonsLibrary.Logging/Logs.cs)
 
 ## My Other C# Libraries
 
